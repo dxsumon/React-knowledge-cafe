4480d75c818ccb76b083a665aaf8aa3ac913f1a4
DX-Sumon <dxsumon14567@gmail.com>
2024-03-15 19:55:20 +0600

    blogs component and tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Blogs from './components/blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl text-center font-bold'>Vite + React</h1>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/blogs/Blogs.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs,setBlogs] = useState([]);
+
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res=> res.json())
+        .then(data=> setBlogs(data))
+    },[])
+    return (
+        <div>
+            <h1>All blog</h1>
+        </div>
+    );
+};
+
+export default Blogs;
